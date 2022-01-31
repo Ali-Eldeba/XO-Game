7df083f344386c1ccccfb55e7b5fb93206a6fe09
Ali Eldeba <alieldeba375@gmail.com>
2022-01-31 21:11:27 +0200

    fixing bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let turn = "x";
 
 let title = document.querySelector(".title");
+let bxs = document.getElementsByClassName("box");
 
 function point(id) {
   if (turn === "x" && document.getElementById(id).innerHTML === "") {
@@ -17,11 +18,14 @@ function point(id) {
     title.innerHTML = "O Turn";
   }
 
+  document.getElementById(id).style.pointerEvents = "none";
+
   check();
+  draw();
 }
 
 function check() {
-  let boxes = [];
+  var boxes = [];
 
   for (i = 1; i < 10; i++) {
     boxes[i] = document.querySelector("#box" + i).innerHTML;
@@ -55,7 +59,49 @@ function check() {
 
 function win(num1, num2, num3) {
   title.innerHTML = `${num1} Won!`;
+
+  // Boxes
+  let box1 = (document.querySelector("#box1").style.pointerEvents = "none");
+  let box2 = (document.querySelector("#box2").style.pointerEvents = "none");
+  let box3 = (document.querySelector("#box3").style.pointerEvents = "none");
+  let box4 = (document.querySelector("#box4").style.pointerEvents = "none");
+  let box5 = (document.querySelector("#box5").style.pointerEvents = "none");
+  let box6 = (document.querySelector("#box6").style.pointerEvents = "none");
+  let box7 = (document.querySelector("#box7").style.pointerEvents = "none");
+  let box8 = (document.querySelector("#box8").style.pointerEvents = "none");
+  let box9 = (document.querySelector("#box9").style.pointerEvents = "none");
+
   setTimeout(function () {
     location.reload();
   }, 1000);
 }
+
+function draw() {
+  let boxes2 = [];
+
+  for (i = 1; i < 10; i++) {
+    boxes2[i] = document.querySelector("#box" + i).innerHTML;
+  }
+
+  if (
+    boxes2[1] != "" &&
+    boxes2[2] != "" &&
+    boxes2[3] != "" &&
+    boxes2[4] != "" &&
+    boxes2[5] != "" &&
+    boxes2[6] != "" &&
+    boxes2[7] != "" &&
+    boxes2[8] != "" &&
+    boxes2[9] != ""
+  ) {
+    title.innerHTML = "Draw!";
+    setTimeout(function () {
+      location.reload();
+    }, 1000);
+  }
+}
+
+// if (state === "game") {
+// } else if (state === "win") {
+
+// }
